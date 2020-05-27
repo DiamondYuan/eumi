e6a890d3bd79b4601e078e574043aabd783f96ba
DiamondYuan <admin@diamondyuan.com>
2020-05-27 21:56:30 +0800

    :heavy_plus_sign: add common-bin

--- src/cli.ts
+++ src/cli.ts
@@ -1,1 +1,16 @@
-console.log("main");
+
+
+import Command from 'common-bin'
+
+class Program extends Command {
+    constructor() {
+        super();
+        this.yargs.scriptName('eumi');
+        this.usage = 'Usage: [command] [options]';
+        this.version = require('../package.json').version;
+    }
+}
+
+
+
+new Program().start();
