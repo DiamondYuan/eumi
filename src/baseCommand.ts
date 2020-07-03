ba8baf49622a26ce0c8b1ef1f328a9b9355d34ac
DiamondYuan <admin@diamondyuan.com>
2020-07-03 23:55:35 +0800

    feat: update copy script

--- src/baseCommand.ts
+++ src/baseCommand.ts
@@ -1,6 +1,10 @@
 import Command from "common-bin";
 
 abstract class BaseCommand extends Command {
+  constructor(rawArgv: string[]) {
+    super(rawArgv);
+  }
+
   abstract _run(context: Command.Context): Promise<void>;
 
   async run(context: Command.Context) {
--- src/commands/copy.ts
+++ src/commands/copy.ts
@@ -1,13 +1,23 @@
 import BaseCommand from "../baseCommand";
 import Command from "common-bin";
+import { resolve } from "path";
 
 class CopyCommand extends BaseCommand {
-  async _run({ env, ...rest }: Command.Context) {
-    console.log("copy", rest);
+  constructor(rawArgv: string[]) {
+    super(rawArgv);
+    this.usage = "Usage: eumi copy source_file target_file";
+  }
+  async _run({ argv, cwd }: Command.Context) {
+    const [source_file, target_file] = argv._;
+    if (!source_file || !target_file) {
+      return this.showHelp();
+    }
+    const sourcePath = resolve(cwd, source_file);
+    const targetPath = resolve(cwd, target_file);
   }
 
   get description() {
-    return "Copy File";
+    return "copy source_file target_file";
   }
 }
 
