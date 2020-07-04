10c51131dd679bd76be1426b5a8def2ae5f97bb5
DiamondYuan <admin@diamondyuan.com>
2020-07-04 23:36:31 +0800

    :sparkles: implements eumi copy

--- src/commands/copy.ts
+++ src/commands/copy.ts
@@ -1,7 +1,7 @@
 import BaseCommand from "../baseCommand";
 import Command from "common-bin";
 import { resolve } from "path";
-
+import { exists, stat, copyFile } from "mz/fs";
 class CopyCommand extends BaseCommand {
   constructor(rawArgv: string[]) {
     super(rawArgv);
@@ -14,6 +14,17 @@ class CopyCommand extends BaseCommand {
     }
     const sourcePath = resolve(cwd, source_file);
     const targetPath = resolve(cwd, target_file);
+    const exist = await exists(sourcePath);
+    if (!exist) {
+      console.log("sourcePath not exist.");
+      return;
+    }
+    const fileStat = await stat(source_file);
+    if (!fileStat.isFile()) {
+      console.log(`copy: ${source_file} is not a file.`);
+      return;
+    }
+    await copyFile(sourcePath, targetPath);
   }
 
   get description() {
