9ca7dd5c83beedd5743d046a8edfd2ca432db58e
DiamondYuan <admin@diamondyuan.com>
2020-07-16 23:50:45 +0800

    :sparkles: add utils to context

--- src/baseCommand.ts
+++ src/baseCommand.ts
@@ -1,10 +1,22 @@
 import Command from 'common-bin';
+import address from 'address';
+
+const utils = {
+  address,
+};
+
+interface EumiContext extends Command.Context {
+  utils: typeof utils;
+}
 
 abstract class BaseCommand extends Command {
   async run(context: Command.Context) {
-    this._run(context);
+    this._run({
+      ...context,
+      utils,
+    });
   }
-  abstract _run(context: Command.Context): Promise<void>;
+  abstract _run(context: EumiContext): Promise<void>;
 }
 
 export default BaseCommand;
