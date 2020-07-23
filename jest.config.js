ad4d10a8d747adb8a4a5f5afd48da6e211885721
DiamondYuan <admin@diamondyuan.com>
2020-07-23 23:23:26 +0800

    :white_check_mark: update test for eumi

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   setupFilesAfterEnv: ['<rootDir>/setupTest.js'],
+  testPathIgnorePatterns: ['<rootDir>/.eumi_test_tmpdir/'],
   testMatch: ['<rootDir>/**/*.spec.ts'],
 };
--- packages/eumi-test/index.spec.ts
+++ packages/eumi-test/index.spec.ts
@@ -1,6 +1,10 @@
 import { spawn } from 'child_process';
 import { join } from 'path';
-import { existsSync } from 'fs';
+import { Server } from 'http';
+import { existsSync, copy, createReadStream, readJSON, writeJSON } from 'fs-extra';
+import getPort from 'get-port';
+import express from 'express';
+import npminstall from 'npminstall';
 
 const fixture = join(__dirname, '__tests__/fixture');
 const example = join(fixture, 'example');
@@ -15,18 +19,62 @@ function build(cwd: string) {
   });
 }
 
-describe('test setupTest.js', () => {
-  it('expect wewe', () => {
-    expect(typeof process.env.CURRENT_EUMI_TMPDIR === 'string').toBeTruthy();
+const eumi = join(process.env.CURRENT_EUMI_TMPDIR!, 'eumi.tgz');
+
+let port: number = 0;
+let server: Server;
+beforeAll(async () => {
+  port = await getPort();
+  const app = express();
+  app.get('/*', function (_req, res) {
+    createReadStream(eumi).pipe(res);
   });
-  it('', () => {
+  await (async () => {
+    return new Promise((r) => {
+      server = app.listen(port, r);
+    });
+  })();
+});
+
+afterAll(() => {
+  server.close();
+});
+
+describe('test setupTest.js', () => {
+  it('expect CURRENT_EUMI_TMPDIR exist', () => {
+    expect(typeof process.env.CURRENT_EUMI_TMPDIR === 'string').toBeTruthy();
     expect(existsSync(process.env.CURRENT_EUMI_TMPDIR!)).toBe(true);
   });
 });
 
+async function copyToRandomPath(source: string) {
+  const randomPath = join(process.env.CURRENT_EUMI_TMPDIR!, Math.random().toString(36).slice(-8));
+  await copy(source, randomPath);
+  const json: any = await readJSON(join(randomPath, 'package.json'));
+  json.devDependencies.eumi = `http://localhost:${port}/eumi.tgz`;
+  await writeJSON(join(randomPath, 'package.json'), json, {
+    spaces: 2,
+  });
+  const noop = () => {
+    //
+  };
+  await npminstall({
+    root: randomPath,
+    registry: 'https://registry.npm.taobao.org',
+    console: {
+      info: noop,
+      error: noop,
+      log: noop,
+      warn: noop,
+    },
+  });
+  return randomPath;
+}
+
 describe('test build project', () => {
   it('expect build success', async () => {
-    const code = await build(example);
-    expect(code).toBe(0);
+    const randomPath = await copyToRandomPath(example);
+    const buildCode = await build(randomPath);
+    expect(buildCode).toBe(0);
   }, 300000);
 });
