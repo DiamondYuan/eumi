f9ad816732a0020672abc036eb874c415f898834
DiamondYuan <admin@diamondyuan.com>
2020-08-19 08:37:41 +0800

    :sparkles: support read config

--- packages/eumi/src/baseCommand.ts
+++ packages/eumi/src/baseCommand.ts
@@ -1,21 +1,41 @@
 import Command from 'common-bin';
 import address from 'address';
+import { exists } from 'mz/fs';
 
 const utils = {
   address,
 };
 
+interface EumiConfig {}
+
+const CONFIG_FILES = ['.eumirc.js'];
+
 interface EumiContext extends Command.Context {
   utils: typeof utils;
+  config: EumiConfig;
 }
 
 abstract class BaseCommand extends Command {
   async run(context: Command.Context) {
+    const configFile = await this.getConfigFilePath();
+    // TODO support ts
+    const config = configFile ? require(configFile) : {};
     await this._run({
       ...context,
       utils,
+      config,
     });
   }
+
+  private async getConfigFilePath(): Promise<string | null> {
+    for (const config of CONFIG_FILES) {
+      if (await exists(config)) {
+        return config;
+      }
+    }
+    return null;
+  }
+
   abstract _run(context: EumiContext): Promise<void>;
 }
 
